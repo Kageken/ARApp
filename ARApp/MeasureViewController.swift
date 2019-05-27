c5b9bd16f3a2b97258f12398de48f81edae92d25
Kageken <kageken@KagekennoMacBook-Pro.local>
2019-05-27 16:31:57 +0900

    First commit

---
+++ ARApp/MeasureViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MeasureViewController.swift
+//  ARApp
+//
+//  Created by 陰山賢太 on 2019/05/27.
+//  Copyright © 2019 Kageken. All rights reserved.
+//
+
+import UIKit
+
+class MeasureViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
